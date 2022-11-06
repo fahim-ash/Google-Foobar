9c7e4574252463e27f7ea2c76348448565b6b526
fahim ashhab <ashhabfahim@gmail.com>
2022-11-06 18:16:01 +0600

    foo

--- chalange2/challange2.1.py
+++ chalange2/challange2.1.py
@@ -1,5 +1,5 @@
 
-l={"1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"}
+# l={"1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"}
 def solution(l):
     parsed = [e.split(".") for e in l]
     x = [map(int, e) for e in parsed]
